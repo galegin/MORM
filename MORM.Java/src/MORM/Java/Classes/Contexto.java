158ba97cdbfd53757ddfe73f9b066c8ed987bd11
galegin <galegin@gmail.com>
2017-10-16 09:03:50 -0300

    Contexto

--- MORM.Java/src/MORM/Java/Classes/Contexto.java
+++ MORM.Java/src/MORM/Java/Classes/Contexto.java
@@ -1,5 +1,7 @@
 package MORM.Java.Classes;
 
+import java.lang.reflect.Field;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -18,7 +20,36 @@ public class Contexto
     
     public void SetValue(ResultSet resultSet, CollectionItem collectionItem)
     {
-    	
+		try {
+	        Field[] fields = collectionItem.getClass().getDeclaredFields();
+	        for (Field field : fields)
+	        {
+	        	field.setAccessible(true);
+	        	String name = field.getName();
+	        	Class<?> type = field.getType();
+	        	
+	            if (!resultSet.wasNull())
+	            	field.set(collectionItem, null);
+	            else if (type.isAssignableFrom(Boolean.class))
+	            	field.set(collectionItem, ".1.S.SIM.T.TRUE.".indexOf(resultSet.getString(name)) > -1);
+	            else if (type.isAssignableFrom(Date.class)) 
+	                field.set(collectionItem, new Date(resultSet.getTimestamp(name).getTime()));
+	            else if (type.isAssignableFrom(Float.class))
+	                field.set(collectionItem, resultSet.getDouble(name));
+	            else if (type.isAssignableFrom(Integer.class))
+	                field.set(collectionItem, resultSet.getInt(name));
+	            else if (type.isAssignableFrom(String.class))
+	                field.set(collectionItem, resultSet.getString(name));
+	            else
+	                field.set(collectionItem, resultSet.getString(name));
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();	        
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
     }
     
     //-- lista
